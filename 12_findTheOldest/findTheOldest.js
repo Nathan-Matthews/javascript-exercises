655deddd3902eeb92609a50dbd8922ec5676a9cb
Nathan-Matthews <nate.matthews24@gmail.com>
2022-10-12 14:20:10 -0400

    finish findTheOldest, added yearOfDeath to be currentYear if alive (kind of morbid lol)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -2,13 +2,20 @@ const findTheOldest = function(arr) {
     // Find the current year.
     const currentYear = (new Date()).getFullYear();
 
-    // Create a sorted array by 
+    // Create a sorted array by age
     const oldest = arr.sort(function(a,b) {
+    if(a.yearOfDeath == null){
+        a.yearOfDeath = currentYear;
+    }
+    if(b.yearOfDeath == null){
+        b.yearOfDeath = currentYear;
+    }
     const lastGuy = a.yearOfDeath - a.yearOfBirth;
     const nextGuy = b.yearOfDeath - b.yearOfBirth;
     return lastGuy > nextGuy ? -1 : 1;
     
     });
+    // Return the first entry of that array
     return oldest[0];
 
 
