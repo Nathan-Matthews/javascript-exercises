94488d3e99fdb670115b2099adfffb8a3507e3f2
Nathan-Matthews <nate.matthews24@gmail.com>
2022-10-12 10:15:43 -0400

    Finish Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (s) {
 
+    //remove all characters that aren't letters
+    s = s.replace(/[^A-Za-z0-9]/g, '');
+
+    //All characters to lower case
+    s = s.toLowerCase();
+
+    //split string to pieces
+    const arrayStrings = s.split("");
+   
+    //reverse the pieces of the array
+    const reverseArray = arrayStrings.reverse();
+ 
+    //rejoin the reversed pieces
+    const joinArray = reverseArray.join("");
+
+    //return the result of the initial array equaling the reversed one
+    return s == joinArray;
 };
 
 // Do not edit below this line
