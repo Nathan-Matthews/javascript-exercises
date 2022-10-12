d46b3392c937d0b128349fd96645a0305aa39fe2
Nathan-Matthews <nate.matthews24@gmail.com>
2022-10-12 10:42:42 -0400

    Finish fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n < 0){
+        return 'OOPS';
+    }
+    if(n==1 || n==2){
+        return 1;
+    }
+    let sum = 0;
+    if(n > 2){
+        sum = fibonacci(n - 1) + fibonacci(n - 2);
+    }
+    return sum;
 
 };
 
