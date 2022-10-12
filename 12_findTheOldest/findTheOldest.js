c1f1a0d1e40670d309da473d2ca5e101aacfd2b9
Nathan-Matthews <nate.matthews24@gmail.com>
2022-10-12 12:06:36 -0400

    Passed first test of findOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    // Find the current year.
+    const currentYear = (new Date()).getFullYear();
+
+    // Create a sorted array by 
+    const oldest = arr.sort(function(a,b) {
+    const lastGuy = a.yearOfDeath - a.yearOfBirth;
+    const nextGuy = b.yearOfDeath - b.yearOfBirth;
+    return lastGuy > nextGuy ? -1 : 1;
+    
+    });
+    return oldest[0];
+
 
 };
 
